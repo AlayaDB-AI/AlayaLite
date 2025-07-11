8bb60a6a9777dd58db99e7e66c2bcbfafef7861b
liangHuang <1163399601@qq.com>
2025-07-11 15:06:26 +0800

    feat: ARM compilation is supported

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
 
 
 class AlayaLiteConan(ConanFile):
@@ -11,6 +11,8 @@ class AlayaLiteConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.user_presets_path = False
+
+        tc.variables["CONAN_USER_MARCH_FLAGS"] = self._get_march_flags()
         tc.generate()
 
         cmake = CMakeDeps(self)
@@ -30,8 +32,24 @@ class AlayaLiteConan(ConanFile):
         self.options["libcoro"].feature_tls = False
         self.options["libcoro"].build_examples = False
         self.options["libcoro"].build_tests = False
-        
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
         self.copy("*.hpp", dst="include", src="include")
+
+    def _get_march_flags(self):
+        os = self.settings.os
+        arch = self.settings.arch
+        compiler = self.settings.compiler
+
+        if compiler == "msvc":
+            if arch == "x86_64":
+                return "/arch:AVX2"
+            elif arch == "armv8":
+                return "/ARM64"
+        elif os in ["Linux", "Macos"]:
+            if arch == "x86_64":
+                return "-march=x86-64"
+            elif arch == "armv8":
+                return "-march=armv8-a"
+        return ""
