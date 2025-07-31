4ef9a5a36b950e2aff2113cdfa6974be7b52a37a
liangHuang <1163399601@qq.com>
2025-07-31 15:27:06 +0800

    fix: fixes CMake configuration, cpp-lint-check workflow and conan integration
    Signed-off-by: liangHuang <1163399601@qq.com>

--- scripts/conan_build/conan_hook.sh
+++ scripts/conan_build/conan_hook.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+set -e
+set -x
 
 output_dir="${1:-build/generator}"
-# 获取当前脚本所在目录（与 Python 的 __file__ 类似）
+
 PWD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_SOURCE_DIR="$(dirname "$(dirname "${PWD_DIR}")")"
 
-# 获取环境变量 CIBW_ARCHS，默认为 x86_64
-arch="${CIBW_ARCHS:-x86_64}"
+arch="${CIBW_ARCHS_LINUX:-x86_64}"
 
-# 拼接 profile 路径
 h_profile="$PWD_DIR/conan_profile.${arch}"
 b_profile="$PWD_DIR/conan_profile.x86_64"
 
-# 执行 conan install 命令
-conan install . \
+conan install ${PROJECT_SOURCE_DIR} \
     --build=missing \
     -pr:h="${h_profile}" \
     -pr:b="${b_profile}" \
