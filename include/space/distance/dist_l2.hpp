dc81f66d85a9bf17d793287ce3a48b504fcb22a6
ScissorJack-ever <1533201944@qq.com>
2025-09-11 08:33:01 +0000

    feat: add thread-safe random integer generator (random.hpp) feat: add function to compute average value (evaluate.hpp) feat: add RaBitQ utilities hpp files for future development feat: add L2 distance computation for RaBitQ (dist_l2.hpp) build: enable Eigen and OpenMP support build: add AVX512F compilation flags

--- include/space/distance/dist_l2.hpp
+++ include/space/distance/dist_l2.hpp
@@ -21,19 +21,17 @@
 #include <iostream>
 #include <ostream>
 #include "dist_config.hpp"
-// #include "utils/rbq_utils/defines.hpp"
+#include "utils/rbq_utils/defines.hpp"
 
 namespace alaya {
 
-// FAST_BEGIN
-// template <typename DataType = float, typename DistanceType = float>
-// inline auto l2_sqr_rabitq(const DataType *__restrict__ x, const DataType *__restrict__ y,
-//                           size_t dim) -> DistanceType {
-//   ConstVectorMap<DataType> v0(x, dim);
-//   ConstVectorMap<DataType> v1(y, dim);
-//   return (v0 - v1).dot(v0 - v1);
-// }
-// FAST_END
+FAST_BEGIN
+template <typename DataType = float, typename DistanceType = float>
+inline auto l2_sqr_rabitq(const DataType *__restrict__ x, const DataType *__restrict__ y,
+                          size_t dim) -> DistanceType {
+  return l2_sqr(x, y, dim);
+}
+FAST_END
 
 FAST_BEGIN
 template <typename DataType = float, typename DistanceType = float>
